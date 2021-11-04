cba3d7818d584aeb690832f9a842f4757cd00050
Gleb Hmarsky <hmarsky.gleb@yandex.ru>
2021-11-05 00:16:14 +0300

    set initial options and file structure

---
+++ src/scripts/app/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/system';
+import React from 'react';
+
+const App = () => {
+  return (
+    <Box className="App">
+      
+    </Box>
+  );
+}
+
+export default App;
--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../src/scripts/app';
 
 test('renders learn react link', () => {
   render(<App />);
